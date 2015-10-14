129174ceda0e3fd5fb9ea359506585f6f0da8e75
KFlash <kflash123@gmail.com>
2015-10-14 19:18:06 +0800

    moved to util folder

--- src/util/isComponent.js
+++ src/util/isComponent.js
@@ -1,3 +1,4 @@
 export default function isComponent(tagName) {
+// FIX ME!! 'xml/xhtml' have upper case latters in tagname. Need to avoid conflict
     return tagName[0] === tagName[0].toUpperCase();
 };
