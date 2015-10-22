6f7df9bd9737f6d0ed13071701c3affd1f5dc96e
Dominic Gannaway <dominic.gannaway@caplin.com>
2015-10-22 12:51:56 +0100

    exposed parseHtml and refactored validateAttributeName

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { setTransformer }   from  './core/transformer';
 import parseTemplateString  from './core/parseTemplateString';
+import parseHtml  from './core/parseHtml';
 import { clearTemplates, getCachingEnabled, setCachingEnabled }   from  './core/cacheControl';
 import React from './transformers/react';
 import Universal from './transformers/universal';
@@ -10,6 +11,7 @@ let templateCaching = false;
 
 t7.setTransformer = setTransformer;
 t7.clearTemplates = clearTemplates;
+t7.parseHtml = parseHtml;
 t7.getCachingEnabled = getCachingEnabled;
 t7.setCachingEnabled = setCachingEnabled;
 t7.Transformers = { React, Universal, Mithril };
--- src/util/validateAttributeName.js
+++ src/util/validateAttributeName.js
@@ -1,4 +1,4 @@
-let xlink = {
+const xlink = {
 	'xlink:actuate': 'actuate',
 	'xlink:arcrole': 'arcrole',
 	'xlink:href': 'href',
@@ -8,45 +8,44 @@ let xlink = {
 	'xlink:type': 'type'
 };
 
-let xml = {
+const xml = {
 	'xml:base': 'base',
 	'xml:id': 'id',
 	'xml:lang': 'lang',
 	'xml:space': 'spac'
 };
 
-// Simplified subset
-let VALID_ATTRIBUTE_NAME_REGEX = /^[a-zA-Z_][a-zA-Z_\.\-\d]*$/,
-    illegalAttributeNameCache = {},
-    validatedAttributeNameCache = {};
+const isInvalidChar = {
+	'(': true,
+	')': true,
+	'/': true,
+	'\\': true,
+	'[': true,
+	']': true,
+	'{': true,
+	'}': true,
+	'0': true,
+	'1': true,
+	'2': true,
+	'3': true,
+	'4': true,
+	'5': true,
+	'6': true,
+	'7': true,
+	'8': true,
+	'9': true,
+	'!': true,
+	'~': true
+};
 
-/**
- * Validate custom attributes
- *
- * @param  {String} name  The boolean attribute name to set.
- */
 function validateAttribute( name ) {
-
-    if ( validatedAttributeNameCache[name] ) {
-        return true;
-    }
-
-    if ( illegalAttributeNameCache[name] ) {
-        return false;
-    }
-    if ( VALID_ATTRIBUTE_NAME_REGEX.test( name ) || 
-
-    // namespace attributes are seen as non-valid, avoid that!
-	    ( xml[name] ) || 
-		( xlink[name] ) ) {
-			
-        validatedAttributeNameCache[name] = true;
-        return true;
-    }
-
-    illegalAttributeNameCache[name] = true;
-
-    return false;
+	for(let i = 0; i < name.length; i++) {
+		let char = name[i];
+		if(isInvalidChar[char]) {
+			return false;
+		}
+	}
+    return true;
 }
 
 export default validateAttribute;
